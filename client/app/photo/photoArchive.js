289741b4916ca8a7cd9e369ee6b8e802379a4b09
Jonathan Ng <jon7ng@gmail.com>
2014-12-09 13:45:33 -0800

    recreated cordova file

---
+++ client/app/photo/photoArchive.js
@@ -0,0 +1,7 @@
+angular.module("mlp.photoArchive", [])
+
+.controller("photoArchiveController", function ($scope, PhotoFactory, $state) {
+  $scope.userPhotoArchive = PhotoFactory.userPhotoArchive;
+
+  console.log($scope.userPhotoArchive);
+});
